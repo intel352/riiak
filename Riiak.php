8255a7b3a64e626d313fce8bea9d7e19a2aaf6ef
Jon Langevin <intel352@gmail.com>
2011-11-23 23:49:36 -0500

    Slight refactoring of transport layer handling

--- Riiak.php
+++ Riiak.php
@@ -41,28 +41,28 @@ class Riiak extends CApplicationComponent {
      * @var string Default: 'riak'
      */
     public $prefix = 'riak';
-    
+
     /**
      * Riak key stream url prefix
      * 
      * @var string Default: 'buckets' 
      */
     public $bucketPrefix = 'buckets';
-    
+
     /**
      * Riak key for secondary indexes implementation
      * 
      * @var string Default: 'index' 
      */
     public $secIndexPrefix = 'index';
-    
+
     /**
      * Riak key stream prefix
      * 
      * @var string 
      */
     public $keyPrefix = 'keys';
-    
+
     /**
      * MapReduce prefix
      *
@@ -113,20 +113,20 @@ class Riiak extends CApplicationComponent {
      * @var \riiak\MapReduce
      */
     protected $_mr;
-    
+
     /**
      *
      * @var \riiak\SecondaryIndex 
      */
     protected $_sIndex;
-    
+
     /**
      * Defines whether to use secondary index or not.
      *  
      * @var bool Default:false
      */
     public $_useSecondaryIndex = true;
-    
+
     /**
      * Riak configuration details
      * 
@@ -135,26 +135,26 @@ class Riiak extends CApplicationComponent {
     public $_riakConfiguration;
 
     /**
-     *  Create transport layer object
+     * Transport layer object
      * 
-     * @var object 
+     * @var \riiak\Transport 
      */
-    public $_transport;
-    
+    protected $_transport;
+
     /**
      * Define transport layer protocol
      * 
      * @var string Default: http
      */
-    public $_TLProtocol = 'HTTP';
-    
+    public $protocol = 'HTTP';
+
     /**
      * Define transport layer processing method.
      * 
-     * @var string Default:Curl
+     * @var string Default: Curl
      */
-    public $_TLProcessingMethod = 'CURL';
-    
+    public $protocolMethod = 'CURL';
+
     /**
      * Initialise Riiak
      */
@@ -165,47 +165,6 @@ class Riiak extends CApplicationComponent {
          */
         if (empty($this->clientId))
             $this->clientId = 'php_' . base64_encode(rand(1, 1073741824));
-        /**
-         * Check if transport layer object is present or not.
-         */
-        if(!is_object($this->_transport)){
-            /**
-             * Create transport layer object.
-             */
-            $this->_transport = $this->createTLObject($this);
-        }
-    }
-    
-    /**
-     * Method to create transport layer object as per the protocol.
-     * 
-     * @param object $objClient
-     * @return Object Transport Layer object
-     */
-    public static function createTLObject(Riiak $objClient){
-        switch($objClient->_TLProtocol){
-            default:
-            case 'HTTP':
-                /**
-                 * Get processing method object for HTTP protocol.
-                 */
-                switch($objClient->_TLProcessingMethod){
-                    default:
-                    case 'CURL':
-                         return new \riiak\transport\http\Curl($objClient);
-                        break;
-                    case 'FOPEN':
-                        break;
-                    case 'PHPSTREAM':
-                        break;
-                }
-                break;
-            case 'PBC':
-                /**
-                 * Protocol Buffer Transport layer class object.
-                 */
-                break;
-        }
     }
 
     /**
@@ -227,7 +186,7 @@ class Riiak extends CApplicationComponent {
         Yii::trace('Fetching list of buckets', 'ext.riiak.Riiak');
         $buckets = array();
         $buckets = $this->_transport->getBuckets($this);
-        return $buckets; 
+        return $buckets;
     }
 
     /**
@@ -241,6 +200,40 @@ class Riiak extends CApplicationComponent {
     }
 
     /**
+     * Returns the Transport instance (created if not exists)
+     *
+     * @return \riiak\Transport
+     */
+    public function getTransport() {
+        if (!($this->_transport instanceof Transport)) {
+            switch ($this->protocol) {
+                default:
+                case 'HTTP':
+                    /**
+                     * Get processing method object for HTTP protocol.
+                     */
+                    switch ($this->protocolMethod) {
+                        default:
+                        case 'CURL':
+                            $this->_transport = new \riiak\transport\http\Curl($this);
+                            break;
+                        case 'FOPEN':
+                            break;
+                        case 'PHPSTREAM':
+                            break;
+                    }
+                    break;
+                case 'PBC':
+                    /**
+                     * Protocol Buffer Transport layer class object.
+                     */
+                    break;
+            }
+        }
+        return $this->_transport;
+    }
+
+    /**
      * Returns the MapReduce instance (created if not exists)
      *
      * @param bool $reset Whether to create a new MapReduce instance
@@ -251,7 +244,7 @@ class Riiak extends CApplicationComponent {
             $this->_mr = new MapReduce($this);
         return $this->_mr;
     }
-    
+
     /**
      * Returns the SecondaryIndex instance (created if not exists)
      *
@@ -260,27 +253,28 @@ class Riiak extends CApplicationComponent {
      */
     public function getSecondaryIndexObject($reset = false) {
         if ($reset || !($this->_sIndex instanceof MapReduce))
-            $this->_sIndex = new SecondaryIndex ($this);
+            $this->_sIndex = new SecondaryIndex($this);
         return $this->_sIndex;
     }
-    
+
     /**
      * Check whether riak supports multi-backend or not.
      * 
      * @return bool
      */
-    public function getIsMultiBackendSupport(){
+    public function getIsMultiBackendSupport() {
         Yii::trace('Checking multi-backend support', 'ext.riiak.Riiak');
         return $this->_transport->getIsMultiBackendSupport();
     }
-    
+
     /**
      *  Check whether riak supports secondary index or not.
      * 
      * @return bool 
      */
-    public function getIsSecondaryIndexSupport(){
+    public function getIsSecondaryIndexSupport() {
         Yii::trace('Checking Secondary Index support', 'ext.riiak.Riiak');
         return $this->_transport->getIsSecondaryIndexSupport();
     }
+
 }
